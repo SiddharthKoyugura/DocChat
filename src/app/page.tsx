7d090e7c53ab57a19f088324c5c266a0e375a538
SiddharthKoyugra <koyugurasiddhardha@gmail.com>
2023-10-01 00:14:13 +0530

    ClerkAuth init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-red-500'>Hello world</h1>
+    <Button> Hello </Button>
   )
 }
