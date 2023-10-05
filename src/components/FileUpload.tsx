a957608c1e2da1728438ed15e7c7b4b74bb79239
SiddharthKoyugra <koyugurasiddhardha@gmail.com>
2023-10-06 01:38:21 +0530

    react query setup

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,45 +1,80 @@
 "use client"
 import { uploadToS3 } from '@/lib/s3';
-import { Inbox } from 'lucide-react';
-import React from 'react'
+import { Inbox, Loader2 } from 'lucide-react';
+import React, { useState } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import toast from 'react-hot-toast';
 
 
 const FileUpload = () => {
-
-  const { getRootProps, getInputProps } = useDropzone({
-    accept: {'application/pdf':[".pdf"]},
-    maxFiles: 1,
-    onDrop: async (acceptedFiles) => {
-        console.log(acceptedFiles);
-        const file = acceptedFiles[0];
-        if(file.size > 10 * 1024 * 1024){
-            // Bigger than 10mb
-            alert('File size should not be greater than 10MB');
-            return;
+    const [ uploading, setUploading ] = useState(false);
+    const { mutate, isLoading } = useMutation({
+        mutationFn: async ({file_key, file_name}: {file_key: string, file_name: string}) => {
+            const response = await axios.post('/api/create-chat', {file_key, file_name});
+            return response.data;
         }
+    });
 
-        try{
-            const data = await uploadToS3(file);
-        }catch(error){
-            console.error(error);
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: { 'application/pdf': [".pdf"] },
+        maxFiles: 1,
+        onDrop: async (acceptedFiles) => {
+            const file = acceptedFiles[0];
+            if (file.size > 10 * 1024 * 1024) {
+                // Bigger than 10mb
+                toast.error("File size should not be greater than 10MB");
+                return;
+            }
+
+            try {
+                setUploading(true);
+                const data = await uploadToS3(file);
+                if(!data?.file_key || !data?.file_name){
+                    toast.error("something went wrong");
+                    return;
+                }
+                mutate(data, {
+                    onSuccess: (data) => {
+                        toast.success(data.message);
+                    },
+                    onError: (err) => {
+                        toast.error("Error creating chat!");
+                    }
+                });
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setUploading(false);
+            }
         }
-    }
-  });
-  return (
-    <div className="p-2 bg-white rounded-xl">
-        <div {...getRootProps({
-            className: 'border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col',
+    });
+    return (
+        <div className="p-2 bg-white rounded-xl">
+            <div {...getRootProps({
+                className: 'border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col',
 
-        })}>
-            <input {...getInputProps()} />
-            <>
-                <Inbox className='w-10 h-10 text-blue-500' />
-                <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
-            </>
+            })}>
+                <input {...getInputProps()} />
+                {(uploading || isLoading) ? (
+                    <>
+                        {/* Loading State */}
+                        <Loader2 className='h-10 w-10 text-blue-500 animate-spin' />
+                        <p className='mt-2 text-sm text-slate-400'>
+                            Spilling Tea to GPT....
+                        </p>
+                    </>
+                ) : (
+                    <>
+                        <Inbox className='w-10 h-10 text-blue-500' />
+                        <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+                    </>
+                )}
+                
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default FileUpload
